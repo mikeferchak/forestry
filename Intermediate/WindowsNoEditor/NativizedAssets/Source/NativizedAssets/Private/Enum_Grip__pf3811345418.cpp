ccc08831b77f8b958e445ebb284cbfdd596b403c
mike ferchak <mikeferchak@gmail.com>
2017-04-02 00:34:12 -0400

    use default mirroring mode for vive and rift

--- Intermediate/WindowsNoEditor/NativizedAssets/Source/NativizedAssets/Private/Enum_Grip__pf3811345418.cpp
+++ Intermediate/WindowsNoEditor/NativizedAssets/Source/NativizedAssets/Private/Enum_Grip__pf3811345418.cpp
@@ -6,9 +6,9 @@ FText E__Enum_Grip__pf__GetUserFriendlyName(int32 InValue)
 	const auto EnumValue = static_cast<E__Enum_Grip__pf>(InValue);
 	switch(EnumValue)
 	{
-		case E__Enum_Grip__pf::NewEnumerator0: FTextStringHelper::ReadFromString(TEXT("NSLOCTEXT(\"[E4418E814EFA401DC8DB27843368F786]\", \"E796EC97480A8E0EDEB463956885CBB9\", \"Open\")"), Text); break;
-		case E__Enum_Grip__pf::NewEnumerator1: FTextStringHelper::ReadFromString(TEXT("NSLOCTEXT(\"[E4418E814EFA401DC8DB27843368F786]\", \"A7335B584E8DC665B9C72C8D1A852A7C\", \"CanGrab\")"), Text); break;
-		case E__Enum_Grip__pf::NewEnumerator2: FTextStringHelper::ReadFromString(TEXT("NSLOCTEXT(\"[E4418E814EFA401DC8DB27843368F786]\", \"710FF6CB4F03D2943469F59CFC5BB1FF\", \"Grab\")"), Text); break;
+		case E__Enum_Grip__pf::NewEnumerator0: FTextStringHelper::ReadFromString(TEXT("NSLOCTEXT(\"[79877996437E0BF8950BD695329ACA20]\", \"A0A4252145020B9F84DA09AB0566A1FE\", \"Open\")"), Text); break;
+		case E__Enum_Grip__pf::NewEnumerator1: FTextStringHelper::ReadFromString(TEXT("NSLOCTEXT(\"[79877996437E0BF8950BD695329ACA20]\", \"FADC0E324DB75033915E51A9BA910A77\", \"CanGrab\")"), Text); break;
+		case E__Enum_Grip__pf::NewEnumerator2: FTextStringHelper::ReadFromString(TEXT("NSLOCTEXT(\"[79877996437E0BF8950BD695329ACA20]\", \"C502678D47CCF623C3BF4DAE579405DA\", \"Grab\")"), Text); break;
 		case E__Enum_Grip__pf::E__Enum_Grip__pf_MAX: FTextStringHelper::ReadFromString(TEXT(""), Text); break;
 		default: ensure(false);
 	};
--- Intermediate/WindowsNoEditor/NativizedAssets/Source/NativizedAssets/Private/GripEnum__pf563933975.cpp
+++ Intermediate/WindowsNoEditor/NativizedAssets/Source/NativizedAssets/Private/GripEnum__pf563933975.cpp
@@ -6,9 +6,9 @@ FText E__GripEnum__pf__GetUserFriendlyName(int32 InValue)
 	const auto EnumValue = static_cast<E__GripEnum__pf>(InValue);
 	switch(EnumValue)
 	{
-		case E__GripEnum__pf::NewEnumerator0: FTextStringHelper::ReadFromString(TEXT("NSLOCTEXT(\"[20CBC244482EC6279A88D89EF141F6D5]\", \"E1D19B8D4D52EF9B76F185ACE382C4F0\", \"Open\")"), Text); break;
-		case E__GripEnum__pf::NewEnumerator1: FTextStringHelper::ReadFromString(TEXT("NSLOCTEXT(\"[20CBC244482EC6279A88D89EF141F6D5]\", \"19A054584EC3D46CDAE5CBB0DE54A518\", \"CanGrab\")"), Text); break;
-		case E__GripEnum__pf::NewEnumerator2: FTextStringHelper::ReadFromString(TEXT("NSLOCTEXT(\"[20CBC244482EC6279A88D89EF141F6D5]\", \"9F9B687949944217868D1493FE0984B5\", \"Grab\")"), Text); break;
+		case E__GripEnum__pf::NewEnumerator0: FTextStringHelper::ReadFromString(TEXT("NSLOCTEXT(\"[7556C4FE4FC1BE594602608805808EB7]\", \"F89912C84F5450959AA679A1A1E13656\", \"Open\")"), Text); break;
+		case E__GripEnum__pf::NewEnumerator1: FTextStringHelper::ReadFromString(TEXT("NSLOCTEXT(\"[7556C4FE4FC1BE594602608805808EB7]\", \"2AC2D7C746A58A292F1DD89E1796677B\", \"CanGrab\")"), Text); break;
+		case E__GripEnum__pf::NewEnumerator2: FTextStringHelper::ReadFromString(TEXT("NSLOCTEXT(\"[7556C4FE4FC1BE594602608805808EB7]\", \"09683B30469A01A5201B0595929148BF\", \"Grab\")"), Text); break;
 		case E__GripEnum__pf::E__GripEnum__pf_MAX: FTextStringHelper::ReadFromString(TEXT(""), Text); break;
 		default: ensure(false);
 	};
